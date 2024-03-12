6693c1dc3234274014cb18731083223aa83e2a77
Ana Buhaescu <buhaescuana@yahoo.com>
2024-03-12 14:49:56 +0200

    Added an appbar and modified the description of the reel

--- lib/pages/reel.dart
+++ lib/pages/reel.dart
@@ -6,91 +6,119 @@ class ReelPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.grey[300],
-        width: 700.0,
-        height: 800.0,
-        child:  Row(
-          children: [
-             Padding(
-              padding:  EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(5.0),
-                        child: Icon(Icons.account_circle),
-                      ),
-                      Text('marinela_cocheta53'),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: GestureDetector(
-                          onTap: () {},
-                          child: Container(
-                            decoration: BoxDecoration(color: Color.fromARGB(255, 241, 239, 239), borderRadius: BorderRadius.circular(5)),
-                            height: 15,
-                            width: 45,
-                            child: Text('Follow'),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10,
-                    ),
-                  SizedBox(
-                    width: 120,
-                    height: 100,
-                    child: Text(
-                      'Mai mereu mi-am dorit sa va arat aceasta reteta de coltunasi, o bucurie '), 
-                  )
-                  ],
-              ),
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[300],
+        title: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(children: [
+            Text('For you', style: TextStyle(fontSize: 25)),
             Padding(
-              padding: const EdgeInsets.only(
-                left: 110, 
-                bottom: 8.0,
-                ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  SizedBox(
-                      width: 50,
-                      height: 50,
-                      child: 
-                      IconButton(
-                        icon: const Icon(Icons.favorite_border), 
-                        onPressed: () {},
-                        )),
-                  SizedBox(
-                      width: 50,
-                      height: 50,
-                      child: 
-                      IconButton(
-                        icon: const Icon(Icons.mode_comment_outlined),
-                        onPressed: () {},
-                        )),
-                  SizedBox(
-                      width: 50, height: 50, 
-                      child: IconButton(
-                        icon: const Icon(Icons.send_sharp),
-                        onPressed: () {},
-                      )),
-                  SizedBox(width: 50, height: 50,
-                   child: IconButton(
-                    icon: const Icon(Icons.more_vert),
-                    onPressed: () {},
-                    ))
-                ],
+              padding: const EdgeInsets.all(8.0),
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.arrow_downward_sharp,
+                size: 20,)
               ),
-            ),
-          ],
+            )
+          ]),
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.camera_alt_outlined))
+        ],
+      ),
+      body: Center(
+        child: Container(
+          color: Colors.grey[300],
+          width: 700.0,
+          height: 800.0,
+          child: Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.all(5.0),
+                          child: Icon(Icons.account_circle),
+                        ),
+                        const Text('marinela_cocheta53'),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  color: Color.fromARGB(255, 241, 239, 239),
+                                  borderRadius: BorderRadius.circular(5)),
+                              height: 15,
+                              width: 45,
+                              child: const Text('Follow'),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        width: 200,
+                        height: 100,
+                        child: Text(
+                            'Mai mereu mi-am dorit sa va arat aceasta reteta de coltunasi, o bucurie '),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 110,
+                  bottom: 8.0,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    SizedBox(
+                        width: 50,
+                        height: 50,
+                        child: IconButton(
+                          icon: const Icon(Icons.favorite_border),
+                          onPressed: () {},
+                        )),
+                    SizedBox(
+                        width: 50,
+                        height: 50,
+                        child: IconButton(
+                          icon: const Icon(Icons.mode_comment_outlined),
+                          onPressed: () {},
+                        )),
+                    SizedBox(
+                        width: 50,
+                        height: 50,
+                        child: IconButton(
+                          icon: const Icon(Icons.send_sharp),
+                          onPressed: () {},
+                        )),
+                    SizedBox(
+                        width: 50,
+                        height: 50,
+                        child: IconButton(
+                          icon: const Icon(Icons.more_vert),
+                          onPressed: () {},
+                        ))
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
